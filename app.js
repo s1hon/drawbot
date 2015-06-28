c520d77019df9bbc4c465f7326f2ed3b1f2b3d86
Sean Chen <opjlmi@gmail.com>
2015-06-28 13:51:06 +0800

    import bootstrap

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var swig = require('swig');
+var bootstrap = require('bootstrap');
 
+// Set routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var app = express();                                                                                                    
 
 // view engine setup
 app.engine('html', swig.renderFile);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource : /users/index');
+});
+
+
+router.get('/:id', function(req, res, next) {
+  res.send('respond with a resource : '+req.params.id);
 });
 
 module.exports = router;
