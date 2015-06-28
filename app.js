271f40a68675e6398de18e8714347de45da2b457
Sean Chen <opjlmi@gmail.com>
2015-06-28 14:12:15 +0800

    fix bootstrap

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var swig = require('swig');
-var bootstrap = require('bootstrap');
 
 // Set routes
 var routes = require('./routes/index');
