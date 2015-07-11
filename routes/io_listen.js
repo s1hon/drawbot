05affe29bbabdba191a05d5424b65c14c44f208b
Sean Chen <opjlmi@gmail.com>
2015-07-11 19:10:07 +0800

    add start camera socket.io

--- routes/io_listen.js
+++ routes/io_listen.js
@@ -7,4 +7,21 @@ module.exports = function(io) {
 	//   });
 	// });
 
+	io.on('connection', function (socket) {
+
+		socket.on('new-print', function (data) {
+			if(data.camera=='open'){
+				console.log('{ request: "Camera open request." }');
+				setTimeout(function(){camera_open(socket);}, 5000);
+			}
+		});
+
+	});
+
+
+	function camera_open(socket){
+		socket.emit('server', {camera: 'ok'});	
+	}
+
+
 };
