6758f540531d4ccbc235d726f20bbf82890c9477
Sean Chen <opjlmi@gmail.com>
2015-07-26 11:30:16 +0800

    add save pic codes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 module.exports = function(app,io){
 
-	var base64 = require('node-base64-image');
+	var fs = require('fs');
 
 	/* GET home page. */
 	app.get('/', function(req, res, next) {
@@ -12,12 +12,22 @@ module.exports = function(app,io){
 		res.render('new-print', { title: '新增列印 | Drawbot' });
 	});
 
+	// 接收canvas的資料，準備將資料轉成圖片再儲存
 	app.post('/upload', function (req, res) {
 		var upload = parseDataURL(req.body.data);
-		console.log(upload);
-		io.emit('server', {server: '[SERVER] Get Picture!'});
+
+		if (upload){
+			io.emit('server', {server: '傳送成功！'});
+			console.log('[SERVER] Get a picture!');
+			savePicture(upload.data);
+		}else{
+			io.emit('server', {ERR: '[ERR] Fail to get picture!'});
+			console.log('[ERR] Server can not get picutre');
+		}
+
 	});
 
+	// 將canvas的資訊轉換成Buffer
 	function parseDataURL(body) {
 		var match = /data:([^;]+);base64,(.*)/.exec(body);
 		if(!match)
@@ -27,6 +37,19 @@ module.exports = function(app,io){
 			contentType: match[1],
 			data: new Buffer(match[2], 'base64')
 		};
+	}
+
+	// 將轉好的圖片存至硬碟中
+	function savePicture(data) {
+		console.log(data);
+		fs.writeFile('public/user_pic/test.png', data, function(err) {
+			if(err){
+				console.log(err);
+			}else{
+				console.log('[SERVER] Save picutre already!');
+			}
+		});
+
 	}
 
 };
